8d6a62961d143a436a50a3c7dbfa49c9a541c55f
Matheus Kur <matheuskur7@gmail.com>
2026-05-04 19:47:28 -0300

    corrigido tamanho do corpo da mensagem para o número de tasks e renomeada função de ElectionStage para TaskProcess

--- ring_base_code_v3.go
+++ ring_base_code_v3.go
@@ -10,7 +10,7 @@ import (
 
 type mensagem struct {
 	tipo  int    // tipo da mensagem para fazer o controle do que fazer (eleição, confirmacao da eleicao)
-	corpo [6]int // conteudo da mensagem para colocar os ids (usar um tamanho compativel com o numero de processos no anel)
+	corpo [4]int // conteudo da mensagem para colocar os ids (usar um tamanho compativel com o numero de processos no anel)
 }
 
 var (
@@ -58,7 +58,7 @@ func ElectionControler(in chan int) {
 	fmt.Println("\n   Processo controlador concluído\n")
 }
 
-func ElectionStage(TaskId int, in chan mensagem, out chan mensagem, leader int) {
+func TaskProcess(TaskId int, in chan mensagem, out chan mensagem, leader int) {
 	defer wg.Done()
 
 	// variaveis locais que indicam se este processo é o lider e se esta ativo
@@ -72,7 +72,7 @@ func ElectionStage(TaskId int, in chan mensagem, out chan mensagem, leader int) 
 	temp := <-in // ler mensagem (só esta consumindo uma mensagem)
 	             // no trabalho precisa ser um laço e funcioar até uma mensagem de término
 				 
-	fmt.Printf("%2d: recebi mensagem %d, [ %d, %d, %d ]\n", TaskId, temp.tipo, temp.corpo[0], temp.corpo[1], temp.corpo[2])
+	fmt.Printf("%2d: recebi mensagem %d, [ %d, %d, %d, %d ]\n", TaskId, temp.tipo, temp.corpo[0], temp.corpo[1], temp.corpo[2], temp.corpo[3])
 
 	switch temp.tipo {
 	case 2:
@@ -105,10 +105,10 @@ func main() {
 
 	// criar os processo do anel de eleicao
 
-	go ElectionStage(0, chans[3], chans[0], 0) // este é o lider
-	go ElectionStage(1, chans[0], chans[1], 0) // não é lider, é o processo 0
-	go ElectionStage(2, chans[1], chans[2], 0) // não é lider, é o processo 0
-	go ElectionStage(3, chans[2], chans[3], 0) // não é lider, é o processo 0
+	go TaskProcess(0, chans[3], chans[0], 0) // este é o lider
+	go TaskProcess(1, chans[0], chans[1], 0) // não é lider, é o processo 0
+	go TaskProcess(2, chans[1], chans[2], 0) // não é lider, é o processo 0
+	go TaskProcess(3, chans[2], chans[3], 0) // não é lider, é o processo 0
 
 	fmt.Println("\n   Anel de processos criado")
 
